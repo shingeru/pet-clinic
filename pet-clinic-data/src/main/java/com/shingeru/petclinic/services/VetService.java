091ff5295313b2df677c6cbe87053142306abae7
norbert <shingeru@gmail.com>
2018-12-02 23:30:24 +0100

    refecoring to common crud interface, closes #11

--- pet-clinic-data/src/main/java/com/shingeru/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/shingeru/petclinic/services/VetService.java
@@ -2,14 +2,7 @@ package com.shingeru.petclinic.services;
 
 import com.shingeru.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 
 }
