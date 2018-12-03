161135dd6e6868ffe1d3de4b52836509ba90c111
norbert <shingeru@gmail.com>
2018-12-03 17:00:24 +0100

    added vets list and controller, closes #15

--- pet-clinic-web/src/main/java/com/shingeru/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/shingeru/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.shingeru.petclinic.controllers;
 
+import com.shingeru.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","vets/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
