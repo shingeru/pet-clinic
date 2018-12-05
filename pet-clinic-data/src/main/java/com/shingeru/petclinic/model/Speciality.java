c8b3d14531eca01b82d9a0dae39b7b1a02e68a90
norbert <shingeru@gmail.com>
2018-12-05 20:00:39 +0100

    update vet with JPA entity mappings, closes #37

--- pet-clinic-data/src/main/java/com/shingeru/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/shingeru/petclinic/model/Speciality.java
@@ -3,9 +3,17 @@ package com.shingeru.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "speciaity")
 public class Speciality extends BaseEntity {
 
+
+    @Column(name = "description")
     private String description;
 }
--- pet-clinic-data/src/main/java/com/shingeru/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/shingeru/petclinic/model/Vet.java
@@ -4,12 +4,20 @@ package com.shingeru.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_spcialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 }
