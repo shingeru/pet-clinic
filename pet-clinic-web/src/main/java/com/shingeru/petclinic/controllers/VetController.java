01ccbcf8e21cab1a7a292bdb59686740218c9df6
norbert <shingeru@gmail.com>
2018-12-20 17:30:40 +0100

    impl vets JSON api, closes #66

--- pet-clinic-web/src/main/java/com/shingeru/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/shingeru/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.shingeru.petclinic.controllers;
 
+import com.shingeru.petclinic.model.Vet;
 import com.shingeru.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,6 +23,12 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetJSON() {
+
+        return vetService.findAll();
     }
 
 }
